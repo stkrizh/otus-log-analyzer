e98437d60222be20b2509c080b182a3f73d53291
stkrizh <stanislav.kiryuhin@gmail.com>
2019-07-13 23:44:13 +0300

    Added function to find the most recent log in specified directory

--- log_analyzer/core.py
+++ log_analyzer/core.py
@@ -11,8 +11,8 @@ LOG_FILENAME_PATTERN = re.compile(r"^nginx-access-ui\.log-(\d{8})\.(gz|log)$")
 LogFile = namedtuple("LogFile", ["path", "date", "extension"])
 
 
-def _most_recent_log(filenames):
-    """Finds the most recent filename in specified list of filenames.
+def _most_recent_filename(filenames):
+    """Finds the most recent filename in specified iterable of filenames.
 
     Parameters
     ----------
@@ -33,3 +33,36 @@ def _most_recent_log(filenames):
         return None
 
     return most_recent_filename
+
+
+def find_most_recent_log(directory):
+    """Finds most recent log in specified directory.
+
+    Parameters
+    ----------
+    directory : str
+        Path to directory with logs.
+
+    Returns
+    -------
+    LogFile
+        Named tuple with full path, date and extension of
+        the most recent log.
+    """
+    if not os.path.isdir(directory):
+        raise ValueError("{0} is not a directory.".format(directory))
+
+    most_recent_filename = _most_recent_filename(os.listdir(directory))
+
+    if most_recent_filename is None:
+        raise ValueError("There are no valid logs.")
+
+    raw_date, extension = LOG_FILENAME_PATTERN.search(
+        most_recent_filename
+    ).groups()
+
+    return LogFile(
+        path=os.path.abspath(os.path.join(directory, most_recent_filename)),
+        date=dt.datetime.strptime(raw_date, "%Y%m%d"),
+        extension=extension,
+    )
