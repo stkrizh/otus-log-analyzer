ae77ab5c86ddf651b79da5ef1ac05f9875d99035
stkrizh <stanislav.kiryuhin@gmail.com>
2019-07-13 23:53:57 +0300

    Test for _most_recent_filename function

--- tests/context.py
+++ tests/context.py
@@ -5,4 +5,4 @@ sys.path.insert(
     0, os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 )
 
-import log_analyzer
+from log_analyzer import core
--- tests/test_core.py
+++ tests/test_core.py
@@ -1,17 +1,33 @@
 import unittest
 
+from context import core
 
-class TestStringMethods(unittest.TestCase):
-    def test_upper(self):
-        self.assertEqual('foo'.upper(), 'FOO')
+
+class TestCore(unittest.TestCase):
+    def test_most_recent_filenames(self):
+        filenames = [
+            "nginx-access-ui.log-20170630.gz",
+            "nginx-access-ui.log-20150629.gz",
+            "nginx-access-ui.log-20150630.bz",
+            "image.jpg",
+            "nginx-access-ui.log-20150630.gz",
+            "nginx-access-ui.log-20170701.log",
+            "nginx-access-ui.log-20170701.gz",
+            "nginx-access-XX.log-20191212.gz",
+        ]
+
+        self.assertEqual(
+            "nginx-access-ui.log-20170701.log",
+            core._most_recent_filename(filenames)
+        )
 
     def test_isupper(self):
-        self.assertTrue('FOO'.isupper())
-        self.assertFalse('Foo'.isupper())
+        self.assertTrue("FOO".isupper())
+        self.assertFalse("Foo".isupper())
 
     def test_split(self):
-        s = 'hello world'
-        self.assertEqual(s.split(), ['hello', 'world'])
+        s = "hello world"
+        self.assertEqual(s.split(), ["hello", "world"])
         # check that s.split fails when the separator is not a string
         with self.assertRaises(TypeError):
             s.split(2)
